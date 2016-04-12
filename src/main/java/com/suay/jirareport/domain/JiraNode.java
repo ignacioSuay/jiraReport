f990452825d483314c3e2e16abd92adebd14f6f5
Ignacio Suay <ignacio.suay@gmail.com>
2016-04-12 18:07:47 +0100

    add jira nodes names

---
+++ src/main/java/com/suay/jirareport/domain/JiraNode.java
@@ -0,0 +1,41 @@
+package com.suay.jirareport.domain;
+
+/**
+ * Created by suay on 1/13/16.
+ */
+public enum JiraNode {
+    TITLE("title"),
+    ITEM("item"),
+    SUMMARY("summary"),
+    PROJECT("project"),
+    LINK("link"),
+    KEY("key"),
+    TYPE("type"),
+    PRIORITY("priority"),
+    STATUS("status"),
+    RESOLUTION("resolution"),
+    ASSIGNEE("assignee"),
+    REPORTER("reporter"),
+    CREATED("created"),
+    UPDATED("updated"),
+    TIME_ORIGINAL_ESTIMATE("timeoriginalestimate"),
+    TIME_ESTIMATE("timeestimate"),
+    CUSTOM_FIELDS("customfields"),
+    CUSTOM_FIELD("customfield"),
+    CUSTOM_FIELD_NAME("customfieldname"),
+    CUSTOM_FIELD_VALUE("customfieldvalue"),
+    SPRINT("Sprint"),
+    EPIC_LINK("Epic Link"),
+    SECONDS("seconds");
+
+
+    private String name;
+
+    JiraNode(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
