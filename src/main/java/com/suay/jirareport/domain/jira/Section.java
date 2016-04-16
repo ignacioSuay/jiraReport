a95f99c8c4b230e0232e65486a218708deb4a1b8
ignacio.suay <ignacio.suay@gmail.com>
2016-04-16 12:38:32 +0100

    send data from the controller to the back-end - first step

--- src/main/java/com/suay/jirareport/domain/jira/Section.java
+++ src/main/java/com/suay/jirareport/domain/jira/Section.java
@@ -1,7 +1,30 @@
 package com.suay.jirareport.domain.jira;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by natxo on 16/04/16.
  */
 public class Section {
+
+    String name;
+
+    Map<String, Boolean> columns = new HashMap<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, Boolean> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(Map<String, Boolean> columns) {
+        this.columns = columns;
+    }
 }
--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -9,6 +9,7 @@
 
     function HomeController ($scope, Principal, LoginService) {
         var vm = this;
+        $scope.sections=[{}];
 
         vm.account = null;
         vm.isAuthenticated = null;
@@ -27,12 +28,32 @@
         }
 
         $scope.reportTables= [
-            {id: '1', name: 'All the issues'},
-            {id: '2', name: 'Epic Summary'},
-            {id: '3', name: 'Story Summay'},
-            {id: '4', name: 'Bugs summary'},
-            {id: '5', name: 'Tasks Summary'},
-            {id: '6', name: 'Issues by owner'}
+            {id: '1', name: 'All the issues', columns:["Key", "title", "type", "priority", "status", "resolution", "created", "updated", "assignee", "reporter", "time original estimate", "time estimate", "Sprint"]},
+            {id: '2', name: 'Epic Summary', columns:["Key", "Title", "time original estimate", "time estimate"]},
+            {id: '3', name: 'Story Summay', columns:["Key", "Title", "time original estimate", "time estimate"]},
+            {id: '4', name: 'Bugs summary', columns:["Key", "Title", "time original estimate", "time estimate"]},
+            {id: '5', name: 'Tasks Summary', columns:["Key", "Title", "time original estimate", "time estimate"]},
+            {id: '6', name: 'Issues by owner', columns:["Key", "title", "type", "priority", "status", "resolution", "created", "updated", "assignee", "reporter", "time original estimate", "time estimate", "Sprint"]}
         ];
+
+        $scope.addSection = function(){
+            $scope.sections.push({});
+        };
+
+        $scope.removeSection = function(index){
+            $scope.sections.splice(index,1);
+        };
+
+
+        $scope.sendData = function(){
+            var reportDTO = {title: $scope.title, author:$scope.authors};
+            reportDTO.sections = [];
+            $scope.sections.forEach(function(sec){
+               reportDTO.sections.push({name: sec.action.name, columns: sec.columns});
+            });
+            alert(JSON.stringify(reportDTO));
+        };
+
+
     }
 })();
