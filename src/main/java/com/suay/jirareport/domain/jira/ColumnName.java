d62ab6b34abeab68b203a2676094f0fb7d55cf5a
Ignacio Suay <ignacio.suay@gmail.com>
2016-04-21 17:48:36 +0100

    Create enum for Column Name

---
+++ src/main/java/com/suay/jirareport/domain/jira/ColumnName.java
@@ -0,0 +1,8 @@
+package com.suay.jirareport.domain.jira;
+
+/**
+ * Created by suay on 4/21/16.
+ */
+public enum ColumnName {
+    KEY, TITLE, TYPE, PRIORITY, STATUS, RESOLUTION, CREATED, UPDATED, ASSIGNEE, REPORTER, TIME_ORIGINAL_ESTIMATE, TIME_ESTIMATE, SPRINT, EPIC;
+}
