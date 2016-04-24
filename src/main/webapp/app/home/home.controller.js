ad5c70957a15386e3d859046d8097cc2995aa648
ignacio.suay <ignacio.suay@gmail.com>
2016-04-24 20:05:27 +0100

    Add default report schema obj

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -27,21 +27,32 @@
             });
         }
 
-        $scope.reportTables= [
-            {id: '1', name: 'All the issues', columns:["Key", "title", "type", "priority", "status", "resolution", "created", "updated", "assignee", "reporter", "time original estimate", "time estimate", "Sprint"]},
-            {id: '2', name: 'Epic Summary', columns:["Key", "Title", "time original estimate", "time estimate"]},
-            {id: '3', name: 'Story Summay', columns:["Key", "Title", "time original estimate", "time estimate"]},
-            {id: '4', name: 'Bugs summary', columns:["Key", "Title", "time original estimate", "time estimate"]},
-            {id: '5', name: 'Tasks Summary', columns:["Key", "Title", "time original estimate", "time estimate"]},
-            {id: '6', name: 'Issues by owner', columns:["Key", "title", "type", "priority", "status", "resolution", "created", "updated", "assignee", "reporter", "time original estimate", "time estimate", "Sprint"]}
+        $scope.reportTables = [
+            {id: '1', name: 'Epic Summary', columns:["Epic Key", "name", "priority", "status", "resolution", "created", "updated", "assignee", "reporter"], groupBy:["time original estimate", "time estimate", "time logged", "number of issues"]},
+            {id: '2', name: 'Story Summay', columns:["Epic Key", "name", "priority", "status", "resolution", "created", "updated", "assignee", "reporter"], groupBy:["time original estimate", "time estimate", "time logged", "number of issues"]},
+            {id: '3', name: 'Issues by owner', columns:["Key", "title", "type", "priority", "status", "resolution", "created", "updated", "assignee", "reporter", "time original estimate", "time estimate", "time logged", "Sprint"]},
+            {id: '4', name: 'Issues by Epic', columns:["Key", "title", "type", "priority", "status", "resolution", "created", "updated", "assignee", "reporter", "time original estimate", "time estimate", "Sprint"]},
+            {id: '5', name: 'Issues by Story', columns:["Key", "title", "type", "priority", "status", "resolution", "created", "updated", "assignee", "reporter", "time original estimate", "time estimate", "Sprint"]},
+            {id: '6', name: 'All the issues', columns:["Key", "title", "type", "priority", "status", "resolution", "created", "updated", "assignee", "reporter", "time original estimate", "time estimate", "Sprint"]}
         ];
 
         $scope.addSection = function(){
-            $scope.sections.push({});
+            $scope.sections.push({action:$scope.reportTables[0]});
         };
 
         $scope.removeSection = function(index){
             $scope.sections.splice(index,1);
+        };
+
+        $scope.defaultSections = function(){
+            $scope.sections = [
+                {action:$scope.reportTables[0], columns:{"name":true, "status":true}, groupBy:{"time original estimate":true, "number of issues":true}},
+                {action:$scope.reportTables[1], columns:{"Key":true, "name":true, "status":true}, groupBy:{"time original estimate":true, "number of issues":true}},
+                {action:$scope.reportTables[2], columns:{"Key":true, "title":true, "type":true, "priority":true, "status":true, "resolution":true, "created":true, "updated":true, "Sprint":true, "time estimate":true}},
+                {action:$scope.reportTables[3], columns:{"Key":true, "title":true, "type":true, "priority":true, "status":true, "resolution":true, "created":true, "updated":true, "Sprint":true, "time estimate":true}},
+
+
+            ];
         };
 
 
