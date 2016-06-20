a16d0214a12266022a72b28faae97c02c28dfcf2
Ignacio Suay <ignacio.suay@gmail.com>
2016-06-20 17:26:29 +0100

    create test method to download file

--- src/main/java/com/suay/jirareport/web/rest/ReportResource.java
+++ src/main/java/com/suay/jirareport/web/rest/ReportResource.java
@@ -6,6 +6,7 @@ import com.codahale.metrics.annotation.Timed;
 import com.suay.jirareport.domain.jira.ReportDTO;
 import com.suay.jirareport.service.ReportService;
 import com.suay.jirareport.web.rest.dto.LoggerDTO;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,8 +15,11 @@ import org.springframework.web.multipart.MultipartFile;
 import org.xml.sax.SAXException;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by suay on 5/24/16.
@@ -24,6 +28,9 @@ import java.io.IOException;
 @RequestMapping("/api")
 public class ReportResource {
 
+    private final Logger log = LoggerFactory.getLogger(ReportResource.class);
+
+
     @Inject
     ReportService reportService;
 
@@ -31,7 +38,7 @@ public class ReportResource {
         method = RequestMethod.POST, consumes = {"multipart/form-data"})
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Timed
-    public @ResponseBody void createReport(@RequestPart("file") MultipartFile file, @RequestPart("reportDTO") ReportDTO reportDTO) {
+    public void createReport(@RequestPart("file") MultipartFile file, @RequestPart("reportDTO") ReportDTO reportDTO) {
         try {
 //            if (!file.isEmpty()) {
 //                try {
@@ -71,5 +78,24 @@ public class ReportResource {
 //            reportService.createWordDocument(file.getInputStream(), reportDTO, "tem");
 
     }
+
+    @RequestMapping(value = "/download", method = RequestMethod.GET)
+    public void getFile(
+        @PathVariable("file_name") String fileName,
+        HttpServletResponse response) {
+        try {
+            // get your file as InputStream
+            File file = new File("/home/suay/ignacioSuay/jiraReport/simple.docx");
+            FileInputStream is = new FileInputStream(file);
+            // copy it to response's OutputStream
+            org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
+            response.flushBuffer();
+        } catch (IOException ex) {
+            log.info("Error writing file to output stream. Filename was '{}'", fileName, ex);
+            throw new RuntimeException("IOError writing file to output stream");
+        }
+
+    }
+
 
 }
