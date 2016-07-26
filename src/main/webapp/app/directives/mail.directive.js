ce54b50f5b9e49ab110e3574cad9a38cfd62168e
Ignacio Suay <ignacio.suay@gmail.com>
2016-07-26 17:21:12 +0100

    create mail directive

---
+++ src/main/webapp/app/directives/mail.directive.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('jiraReportApp')
+    .directive('suayMail', function () {
+        return {
+            restrict: 'E',
+            scope:false, //use parecent scope
+            templateUrl: 'app/directives/mail.template.html',
+            link: function($scope, $http){
+                $scope.emailSent= false;
+                $scope.email = {name:"", email:"",phone:"", message:"", uuid:$scope.uuid};
+
+                $(function() {
+                    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+                        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+                    }).on("focus", ".floating-label-form-group", function() {
+                        $(this).addClass("floating-label-form-group-with-focus");
+                    }).on("blur", ".floating-label-form-group", function() {
+                        $(this).removeClass("floating-label-form-group-with-focus");
+                    });
+                });
+
+                $scope.sendMeEmail = function(){
+                    alert($scope.email.uuid);
+                    $http.post("http://ec2-52-18-48-89.eu-west-1.compute.amazonaws.com:8080/email-spring/api/sendMeEmail", $scope.email).success(function(data){
+                        $scope.emailSent= true;
+                    });
+                }
+            }
+        }
+    });
